0e7d71d6123d03489a136d8dc26aac0509bd6e34
dosarudaniel <dosarudaniel@gmail.com>
2020-01-21 14:15:42 +0100

    Keep FingerEntry unexported, add comments for id meaning

--- types/FingerTable.go
+++ types/FingerTable.go
@@ -3,15 +3,20 @@ package types
 import "fmt"
 
 type FingerEntry struct {
-	Id   			[]byte
-	GossiperAddress string
+	// ID hash of (n + 2^i) % 2^m
+	// n is the current node
+	// i the index in the finger table i can one of the following values: 0,1,2,3,...,m-1
+	// m is the number of bits
+	id   			[]byte
+	gossiperAddress string
+
 }
 
 type FingerTable []FingerEntry
 
 
 func (fe FingerEntry) String() string {
-	return fmt.Sprintf("FingerEntry: Content{%v - %v}", fe.Id, fe.GossiperAddress)
+	return fmt.Sprintf("FingerEntry: Content{%v - %v}", fe.id, fe.gossiperAddress)
 }
 
 func (ft FingerTable) String() string {
