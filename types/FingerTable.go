bb832b87c9dc84ead4f22b3ccf20c3535a70b37a
dosarudaniel <dosarudaniel@gmail.com>
2020-01-21 14:20:34 +0100

    change id type from []byte to string

--- types/FingerTable.go
+++ types/FingerTable.go
@@ -7,7 +7,7 @@ type FingerEntry struct {
 	// n is the current node
 	// i the index in the finger table i can one of the following values: 0,1,2,3,...,m-1
 	// m is the number of bits
-	id   			[]byte
+	id   			string // hexstring of size m bits, eg. "3fde56" of size m=32
 	gossiperAddress string
 
 }
@@ -21,9 +21,9 @@ func (fe FingerEntry) String() string {
 
 func (ft FingerTable) String() string {
 	outputString := ""
-	// for entry in *FingerTable {
-	// 	outputString +=
+	// for entry in FingerTable {
+	// 	outputString += entry.id + " <-> " + entry.gossiperAddress
 	// }
 
-	return outputString //fmt.Sprintf("FingerTable Content{TODO}")
+	return outputString
 }
