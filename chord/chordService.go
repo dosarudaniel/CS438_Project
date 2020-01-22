966081c8dae92e3f692e26da8a790a88b3895e70
Ulugbek Abdullaev <ulugbekna@gmail.com>
2020-01-22 22:34:40 +0100

    implement Notify

--- chord/chordService.go
+++ chord/chordService.go
@@ -3,6 +3,7 @@ package chord
 
 import (
 	"context"
+	"errors"
 	"github.com/golang/protobuf/ptypes/empty"
 	"google.golang.org/grpc"
 )
@@ -34,6 +35,22 @@ func (chordNode *ChordNode) getStubFor(ip ipAddr) (ChordClient, error) {
 func (chordNode *ChordNode) FindSuccessor(ctx context.Context, in *ID) (*Nodes, error) {
 	return nil, nil
 }
-func (chordNode *ChordNode) Notify(ctx context.Context, in *Node) (*empty.Empty, error) {
-	return nil, nil
+
+func (chordNode *ChordNode) Notify(ctx context.Context, n0 *Node) (*empty.Empty, error) {
+	// n0 thinks it might be our predecessor.
+	// n.notify(n0)
+	//	 if (predecessor is nil or n0 is_in (predecessor; n))
+	//	 predecessor = n0;
+	if n0 == nil {
+		return &empty.Empty{}, errors.New("trying to notify nil node")
+	}
+	chordNode.predecessor.Lock()
+	defer chordNode.predecessor.Unlock()
+	predecessorID := chordNode.predecessor.node.Id
+	n0ID := n0.Id
+	nID := chordNode.node.Id
+	if chordNode.predecessor.node == nil || (predecessorID < n0ID && n0ID < nID) {
+		chordNode.predecessor.node = n0
+	}
+	return &empty.Empty{}, nil
 }
