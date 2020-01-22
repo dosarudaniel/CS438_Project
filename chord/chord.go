f5d0dd3578a44dce1ba51dc50cb261aee978dd58
Ulugbek Abdullaev <ulugbekna@gmail.com>
2020-01-22 13:45:48 +0100

    chord.go:   - add initial version of chord.go   - add IChordNode interface, which defines all of the functions that Chord will have (which I know at this moment)   - add 'Create' fn from the paper; the pseudocode given at the beginning of the function's body     Note: I modified the algorithm to support successor lists: '[n]' means a list that includes only 'n'.
    fingerTable.go:
      - create fingerTable and fingerTableWithMux struct types
      Note: every variable that must be mutexed should come in a struct along with an RWMutex variable as in fingerTableWithMux
    
    helpers.go:
      - the purpose of the file is given in the file (please, read)
      - add nodeWithMux and successorsListWithMux struct types

--- chord/chord.go
+++ chord/chord.go
@@ -1,5 +1,53 @@
 package chord
 
-type Chord struct {
-	// TODO add Chord service support
+import (
+	"context"
+	. "github.com/dosarudaniel/CS438_Project/services/chord_service"
+	"google.golang.org/grpc"
+	"log"
+)
+
+// IChordNode defines all of the functions that Chord will have (which I know at this moment)
+type IChordNode interface {
+	ChordServer
+	Create()
+	Join(Node)
+	ClosestPrecedingNode(ID) Node
+	Notify(Node)
+	StabilizeDaemon()
+	FixFingersDaemon()
+	CheckPredecessorsDaemon()
+}
+
+// Chord node can be created using `NewChordNode`
+// However, it must be followed
+// either by creating own Chord network using `create`
+// or by joining an existing network using `join`
+// Both `create` and `join` must be followed by a call to `launchFingerTableDaemons`
+type ChordNode struct {
+	node Node
+
+	predecessor    nodeWithMux
+	successorsList successorsListWithMux
+
+	fingerTable fingerTableWithMux
+
+	chordServer *grpc.Server
+}
+
+// Create creates a Chord ring
+func (chordNode *ChordNode) Create() {
+	// create a new Chord ring.
+	// n.create()
+	//   predecessor = nil;
+	//	 successor = [n];
+	chordNode.predecessor.Lock()
+	chordNode.predecessor.node = nil
+	chordNode.predecessor.Unlock()
+
+	chordNode.successorsList.Lock()
+	chordNode.successorsList = successorsListWithMux{
+		successorsList: []Node{chordNode.node},
+	}
+	chordNode.successorsList.Unlock()
 }
