cb84285532fa210a9f1478b190eb5476bf454c17
Ulugbek Abdullaev <ulugbekna@gmail.com>
2020-01-22 21:27:54 +0100

    - create ipAddr type alias for string to explicitly annotate ip addresses - change typing of stubsPoolWithMux - add hashString to hash a string and return its hash as a string (might be unnecessary)

--- chord/helpers.go
+++ chord/helpers.go
@@ -1,10 +1,16 @@
 package chord
 
-import "sync"
+import (
+	"crypto"
+	"encoding/hex"
+	"sync"
+)
 import . "github.com/dosarudaniel/CS438_Project/services/chord_service"
 
 // In this file, we put all helper functions and types used by Chord package
 // "helper" can be defined as something that is not worth own file/package and has tiny role in the chord functionality
+
+type ipAddr string
 
 type nodeWithMux struct {
 	node *Node
@@ -14,4 +20,20 @@ type nodeWithMux struct {
 type successorsListWithMux struct {
 	list []Node
 	sync.RWMutex
+}
+
+type ipToStubMap map[ipAddr]ChordClient
+type stubsPoolWithMux struct {
+	pool ipToStubMap
+	sync.RWMutex
+}
+
+func hashString(s string) (string, error) {
+	sha256 := crypto.SHA256.New()
+	_, err := sha256.Write([]byte(s))
+	if err != nil {
+		return "", err
+	}
+	hash := sha256.Sum(nil)
+	return hex.EncodeToString(hash), nil
 }
