947eab4084976a92fcaa6e3b39721d5fb891ee86
Ulugbek Abdullaev <ulugbekna@gmail.com>
2020-01-22 21:26:10 +0100

    chord_messages.proto:   - move message ID to the top, since it's simple   - add message Nodes to represent an array of Nodes
    chord_service.proto:
      - fix FindSuccessor type to return Nodes (successor list)
      - add rpc Notify (read Chord paper)

--- services/chord_service/chord_service.pb.go
+++ services/chord_service/chord_service.pb.go
@@ -7,6 +7,7 @@ import (
 	context "context"
 	fmt "fmt"
 	proto "github.com/golang/protobuf/proto"
+	empty "github.com/golang/protobuf/ptypes/empty"
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
@@ -29,15 +30,18 @@ func init() {
 }
 
 var fileDescriptor_89c86567def1976f = []byte{
-	// 113 bytes of a gzipped FileDescriptorProto
+	// 162 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xd2, 0x2a, 0x4e, 0x2d, 0x2a,
 	0xcb, 0x4c, 0x4e, 0x2d, 0xd6, 0x4f, 0xce, 0xc8, 0x2f, 0x4a, 0x89, 0x87, 0x72, 0x51, 0x79, 0x7a,
 	0x05, 0x45, 0xf9, 0x25, 0xf9, 0x42, 0xac, 0x60, 0x41, 0x29, 0x6d, 0xbc, 0x5a, 0x72, 0x53, 0x8b,
-	0x8b, 0x13, 0xd3, 0x53, 0x8b, 0x21, 0x7a, 0x8c, 0x0c, 0xb8, 0x58, 0x9d, 0x41, 0xe2, 0x42, 0xea,
-	0x5c, 0xbc, 0x6e, 0x99, 0x79, 0x29, 0xc1, 0xa5, 0xc9, 0xc9, 0xa9, 0xc5, 0xc5, 0xf9, 0x45, 0x42,
-	0x9c, 0x7a, 0x60, 0x0d, 0x7a, 0x9e, 0x2e, 0x52, 0xdc, 0x50, 0xa6, 0x5f, 0x7e, 0x4a, 0x6a, 0x12,
-	0x1b, 0x58, 0xa3, 0x31, 0x20, 0x00, 0x00, 0xff, 0xff, 0xc6, 0xb3, 0xbe, 0xc6, 0x9a, 0x00, 0x00,
-	0x00,
+	0x8b, 0x13, 0xd3, 0x53, 0x8b, 0x21, 0x7a, 0xa4, 0xa4, 0xd3, 0xf3, 0xf3, 0xd3, 0x73, 0x52, 0xf5,
+	0xc1, 0xbc, 0xa4, 0xd2, 0x34, 0xfd, 0xd4, 0xdc, 0x82, 0x92, 0x4a, 0x88, 0xa4, 0x51, 0x02, 0x17,
+	0xab, 0x33, 0x48, 0x93, 0x90, 0x06, 0x17, 0xaf, 0x5b, 0x66, 0x5e, 0x4a, 0x70, 0x69, 0x72, 0x72,
+	0x6a, 0x71, 0x71, 0x7e, 0x91, 0x10, 0xa7, 0x1e, 0xd8, 0x34, 0x3d, 0x4f, 0x17, 0x29, 0x1e, 0x28,
+	0xd3, 0x2f, 0x3f, 0x25, 0xb5, 0x58, 0x48, 0x97, 0x8b, 0xcd, 0x2f, 0xbf, 0x24, 0x33, 0xad, 0x52,
+	0x88, 0x1b, 0x49, 0x5c, 0x4a, 0x4c, 0x0f, 0x62, 0x8f, 0x1e, 0xcc, 0x1e, 0x3d, 0x57, 0x90, 0x3d,
+	0x49, 0x6c, 0x60, 0xbe, 0x31, 0x20, 0x00, 0x00, 0xff, 0xff, 0xa7, 0xb2, 0x69, 0x94, 0xe7, 0x00,
+	0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -52,7 +56,8 @@ const _ = grpc.SupportPackageIsVersion4
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type ChordClient interface {
-	FindSuccessor(ctx context.Context, in *ID, opts ...grpc.CallOption) (*Node, error)
+	FindSuccessor(ctx context.Context, in *ID, opts ...grpc.CallOption) (*Nodes, error)
+	Notify(ctx context.Context, in *Node, opts ...grpc.CallOption) (*empty.Empty, error)
 }
 
 type chordClient struct {
@@ -63,9 +68,18 @@ func NewChordClient(cc *grpc.ClientConn) ChordClient {
 	return &chordClient{cc}
 }
 
-func (c *chordClient) FindSuccessor(ctx context.Context, in *ID, opts ...grpc.CallOption) (*Node, error) {
-	out := new(Node)
+func (c *chordClient) FindSuccessor(ctx context.Context, in *ID, opts ...grpc.CallOption) (*Nodes, error) {
+	out := new(Nodes)
 	err := c.cc.Invoke(ctx, "/chord.Chord/FindSuccessor", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *chordClient) Notify(ctx context.Context, in *Node, opts ...grpc.CallOption) (*empty.Empty, error) {
+	out := new(empty.Empty)
+	err := c.cc.Invoke(ctx, "/chord.Chord/Notify", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -74,15 +88,19 @@ func (c *chordClient) FindSuccessor(ctx context.Context, in *ID, opts ...grpc.Ca
 
 // ChordServer is the server API for Chord service.
 type ChordServer interface {
-	FindSuccessor(context.Context, *ID) (*Node, error)
+	FindSuccessor(context.Context, *ID) (*Nodes, error)
+	Notify(context.Context, *Node) (*empty.Empty, error)
 }
 
 // UnimplementedChordServer can be embedded to have forward compatible implementations.
 type UnimplementedChordServer struct {
 }
 
-func (*UnimplementedChordServer) FindSuccessor(ctx context.Context, req *ID) (*Node, error) {
+func (*UnimplementedChordServer) FindSuccessor(ctx context.Context, req *ID) (*Nodes, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method FindSuccessor not implemented")
+}
+func (*UnimplementedChordServer) Notify(ctx context.Context, req *Node) (*empty.Empty, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Notify not implemented")
 }
 
 func RegisterChordServer(s *grpc.Server, srv ChordServer) {
@@ -107,6 +125,24 @@ func _Chord_FindSuccessor_Handler(srv interface{}, ctx context.Context, dec func
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Chord_Notify_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(Node)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ChordServer).Notify(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/chord.Chord/Notify",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ChordServer).Notify(ctx, req.(*Node))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _Chord_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "chord.Chord",
 	HandlerType: (*ChordServer)(nil),
@@ -114,6 +150,10 @@ var _Chord_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "FindSuccessor",
 			Handler:    _Chord_FindSuccessor_Handler,
+		},
+		{
+			MethodName: "Notify",
+			Handler:    _Chord_Notify_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
