9bf4c71ebc2f56edd7800e32ff2442169d45b499
Ulugbek Abdullaev <ulugbekna@gmail.com>
2020-01-24 11:29:54 +0100

    move types from 'helpers.go' to new file 'chordTypes.go'

--- chord/helpers.go
+++ chord/helpers.go
@@ -3,31 +3,10 @@ package chord
 import (
 	"crypto"
 	"encoding/hex"
-	"sync"
 )
-import . "github.com/dosarudaniel/CS438_Project/services/chord_service"
 
 // In this file, we put all helper functions and types used by Chord package
 // "helper" can be defined as something that is not worth own file/package and has tiny role in the chord functionality
-
-type nodeID string
-type ipAddr string
-
-type nodeWithMux struct {
-	nodePtr *Node
-	sync.RWMutex
-}
-
-type successorsListWithMux struct {
-	list []*Node
-	sync.RWMutex
-}
-
-type ipToStubMap map[ipAddr]ChordClient
-type stubsPoolWithMux struct {
-	pool ipToStubMap
-	sync.RWMutex
-}
 
 func hashString(s string) (string, error) {
 	sha256 := crypto.SHA256.New()
