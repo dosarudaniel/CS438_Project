b10628af412287a83eb5d42cce4e9c29cbaca83e
dosarudaniel <dosarudaniel@gmail.com>
2020-01-25 21:02:03 +0100

    Added test file for TransferFile function

--- test2.sh
+++ test2.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 
+# Test the TransferFile function: send 2 chunks from sender to receiver "Hello, " and "World!"
+
 go build
 
-xterm -title "Node 1 219 " -hold -e "./CS438_Project -name A -ipAddr=127.0.0.1:5000 -create -m 8 -v"  &
-sleep 40
-xterm -title "Node 2 33" -hold -e "./CS438_Project -name B -ipAddr=127.0.0.1:5001 -join -existingNodeIp 127.0.0.1:5000 -existingNodeId db -m 8 -v"  &
+xterm -title "Node Sender 219 " -hold -e "./CS438_Project -name A -ipAddr=127.0.0.1:5000 -create -m 8 -v"  &
+sleep 1
+xterm -title "Node Receiver 33" -hold -e "./CS438_Project -name B -ipAddr=127.0.0.1:5001 -join -existingNodeIp 127.0.0.1:5000 -existingNodeId f98eeff24e2fced1a1336182a3e8775326262914cc4087066d9346431795ccdb -m 8 -v -role=2"  &
