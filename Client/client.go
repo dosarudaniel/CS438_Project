eebc787e126067e0d338c60735b31cea5c84f9d4
dosarudaniel <dosarudaniel@gmail.com>
2020-01-25 23:51:26 +0100

    Fixed runtime error

--- Client/client.go
+++ Client/client.go
@@ -28,8 +28,8 @@ func requestFile(client clientService.ClientServiceClient, fileMetadata *clientS
 
 func main() {
 	peersterAddress := flag.String("PeersterAddress", "127.0.0.1:5000", "Peerster address to connect to")
-	file := flag.String("file", "", "file name at owner")
-	ownersID := flag.String("ownersID", "", "File owner's ID")
+	file := flag.String("file", "file1.txt", "file name at owner")
+	ownersID := flag.String("ownersID", "f98eeff24e2fced1a1336182a3e8775326262914cc4087066d9346431795ccdb", "File owner's ID")
 	nameToStore := flag.String("nameToStore", *file, "Name used to store the download file")
 	// TODO: Add upload a file command
 	verbose := flag.Bool("v", false, "verbose mode")
@@ -45,7 +45,7 @@ func main() {
 		log.Level = logger.DebugLevel
 	}
 
-	conn, err := grpc.Dial(*peersterAddress, grpc.WithBlock())
+	conn, err := grpc.Dial(*peersterAddress, grpc.WithBlock(),  grpc.WithInsecure(),)
 	if err != nil {
 		log.Fatal(fmt.Sprint("Fail to dial: %v", err))
 	}
