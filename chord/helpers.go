0eeb3f0fdb4a83213224ea904be230877e192da3
Ulugbek Abdullaev <ulugbekna@gmail.com>
2020-01-26 11:40:10 +0100

    add isBetweenTwoNodesExclusive()

--- chord/helpers.go
+++ chord/helpers.go
@@ -3,6 +3,7 @@ package chord
 import (
 	"crypto"
 	"encoding/hex"
+	. "github.com/dosarudaniel/CS438_Project/services/chord_service"
 )
 
 // In this file, we put all helper functions and types used by Chord package
@@ -16,4 +17,26 @@ func hashString(s string, numOfBitsInID int) (string, error) {
 	}
 	hash := sha256.Sum(nil)
 	return hex.EncodeToString(hash[len(hash)-(numOfBitsInID/8):]), nil
+}
+
+/*
+  For the Chord ring:
+	m is_in (l, r) means l < m & m < r when r > l
+	m is_in (l, r) means l < m         when r < l
+*/
+func isBetweenTwoNodesExclusive(leftmostNode, nodeBetween, rightmostNode Node) bool {
+	l := leftmostNode.Id
+	m := nodeBetween.Id
+	r := rightmostNode.Id
+
+	switch {
+	case l == r: // because interval is exclusive, for which l == r means, it's essentially empty
+		return false
+	case r > l && l < m && m < r:
+		return true
+	case r < l && l < m:
+		return true
+	default:
+		return false
+	}
 }
