f51643424e4c719a5ae00af15587041045757b01
dosarudaniel <dosarudaniel@gmail.com>
2020-01-26 16:21:19 +0100

    Added complete FileSharing test

--- test2.sh
+++ test2.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 
-# Test the TransferFile function: send 2 chunks from sender to receiver "Hello, " and "World!"
+# Test the FileSharing service and Client interaction
 
 go build
 
+rm _Download/file1.txt
+
 xterm -title "Node Sender 219 " -hold -e "./CS438_Project -name A -ipAddr=127.0.0.1:5000 -create -m 8 -v"  &
 sleep 1
-xterm -title "Node Receiver 33" -hold -e "./CS438_Project -name B -ipAddr=127.0.0.1:5001 -join -existingNodeIp 127.0.0.1:5000 -existingNodeId f98eeff24e2fced1a1336182a3e8775326262914cc4087066d9346431795ccdb -m 8 -v -role=2"  &
+cd Client
+go build
+./Client -PeersterAddress=127.0.0.1:5000 -file=file1.txt -ownersID=f98eeff24e2fced1a1336182a3e8775326262914cc4087066d9346431795ccdb -v
+cd ..
+echo "Diference between uploaded and downloaded:"
+diff _Upload/file1.txt _Download/file1.txt
