6a35567b6d8b3531ade5eafc842a904d05dc1db7
dosarudaniel <dosarudaniel@gmail.com>
2020-01-27 03:21:17 +0100

    Reorganized code in client.go + updated test2.go

--- client/client.go
+++ client/client.go
@@ -7,6 +7,7 @@ import (
 	"github.com/dosarudaniel/CS438_Project/logger"
 	clientService "github.com/dosarudaniel/CS438_Project/services/client_service"
 	"google.golang.org/grpc"
+	"os"
 	"time"
 )
 
@@ -29,10 +30,10 @@ func requestFile(client clientService.ClientServiceClient, fileMetadata *clientS
 func main() {
 
 	peersterAddress := flag.String("PeersterAddress", "", "Peerster address to connect to")
+	command := flag.String("command", "" , "Command to be sent to Peerster: download/upload/findSuccessor")
 	file := flag.String("file", "", "file name at owner")
 	ownersID := flag.String("ownersID", "", "File owner's ID")
 	nameToStore := flag.String("nameToStore", "", "Name used to store the download file")
-	// TODO: Add in a future PR: upload a file, add findIP(ID) maybe
 	verbose := flag.Bool("v", false, "verbose mode")
 
 	flag.Parse()
@@ -44,22 +45,52 @@ func main() {
 
 	// Safety checks
 	if *peersterAddress == "" {
-		fmt.Println("No PeersterAddress given")
+		log.Fatal("No PeersterAddress given")
+		os.Exit(-1)
 	}
+
+	switch {
+	case *command == "download":
+		// Safety checks
+		if *file == "" { 			// mandatory
+			log.Fatal("Download: No file name given. Specify which file do you request.")
+		}
+		if *ownersID == "" { 		// mandatory
+			log.Fatal("Download: No ID given. Specify the file owner's ID.")
+		}
+		if *nameToStore == "" {  	// optional
+			log.Info("Download: No nameToStore given. Using " + *file + " name to store it.")
+			*nameToStore = *file
+		}
+
+		log.Info("Sending a download request to " + *peersterAddress)
+
+		fileMetadata := &clientService.FileMetadata{FilenameAtOwner: *file, OwnersID: *ownersID, NameToStore: *nameToStore}
+
+		conn, err := grpc.Dial(*peersterAddress, grpc.WithBlock(), grpc.WithInsecure())
+		if err != nil {
+			fmt.Println(fmt.Sprint("Fail to dial: %v", err))
+		}
+		defer conn.Close()
+
+		client := clientService.NewClientServiceClient(conn)
+
+		err = requestFile(client, fileMetadata, log)
+		if err != nil {
+			fmt.Println(fmt.Sprint("Fail to requestFile: %v", err))
+		}
+
+	case *command == "upload":
+		log.Info("Sending an upload request to " + *peersterAddress)
+
+	case *command == "findSuccessor":
+		log.Info("Sending a findSuccessor request to " + *peersterAddress)
+
+	default:
+		log.Fatal(fmt.Sprintf("No correct command given, try one of the following download/upload/findSuccessor"))
+		os.Exit(-1)
+	}
+
 	// TODO Add more safety checks in a future PR
 
-	fileMetadata := &clientService.FileMetadata{FilenameAtOwner: *file, OwnersID: *ownersID, NameToStore: *nameToStore}
-
-	conn, err := grpc.Dial(*peersterAddress, grpc.WithBlock(), grpc.WithInsecure())
-	if err != nil {
-		fmt.Println(fmt.Sprint("Fail to dial: %v", err))
-	}
-	defer conn.Close()
-
-	client := clientService.NewClientServiceClient(conn)
-
-	err = requestFile(client, fileMetadata, log)
-	if err != nil {
-		fmt.Println(fmt.Sprint("Fail to requestFile: %v", err))
-	}
 }
