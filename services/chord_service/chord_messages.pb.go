249c703eef170e1f5243b6255ab139d0bc22ee87
Ulugbek Abdullaev <ulugbekna@gmail.com>
2020-01-27 22:10:22 +0100

    add DHT support to chord service proto files

--- services/chord_service/chord_messages.pb.go
+++ services/chord_service/chord_messages.pb.go
@@ -145,10 +145,186 @@ func (m *Nodes) GetNodeArray() []*Node {
 	return nil
 }
 
+type Key struct {
+	Key                  string   `protobuf:"bytes,1,opt,name=key,proto3" json:"key,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *Key) Reset()         { *m = Key{} }
+func (m *Key) String() string { return proto.CompactTextString(m) }
+func (*Key) ProtoMessage()    {}
+func (*Key) Descriptor() ([]byte, []int) {
+	return fileDescriptor_ec7364b073ade720, []int{3}
+}
+
+func (m *Key) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_Key.Unmarshal(m, b)
+}
+func (m *Key) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_Key.Marshal(b, m, deterministic)
+}
+func (m *Key) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_Key.Merge(m, src)
+}
+func (m *Key) XXX_Size() int {
+	return xxx_messageInfo_Key.Size(m)
+}
+func (m *Key) XXX_DiscardUnknown() {
+	xxx_messageInfo_Key.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_Key proto.InternalMessageInfo
+
+func (m *Key) GetKey() string {
+	if m != nil {
+		return m.Key
+	}
+	return ""
+}
+
+type Val struct {
+	Val                  string   `protobuf:"bytes,1,opt,name=val,proto3" json:"val,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *Val) Reset()         { *m = Val{} }
+func (m *Val) String() string { return proto.CompactTextString(m) }
+func (*Val) ProtoMessage()    {}
+func (*Val) Descriptor() ([]byte, []int) {
+	return fileDescriptor_ec7364b073ade720, []int{4}
+}
+
+func (m *Val) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_Val.Unmarshal(m, b)
+}
+func (m *Val) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_Val.Marshal(b, m, deterministic)
+}
+func (m *Val) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_Val.Merge(m, src)
+}
+func (m *Val) XXX_Size() int {
+	return xxx_messageInfo_Val.Size(m)
+}
+func (m *Val) XXX_DiscardUnknown() {
+	xxx_messageInfo_Val.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_Val proto.InternalMessageInfo
+
+func (m *Val) GetVal() string {
+	if m != nil {
+		return m.Val
+	}
+	return ""
+}
+
+type KeyVal struct {
+	Key                  string   `protobuf:"bytes,1,opt,name=key,proto3" json:"key,omitempty"`
+	Val                  string   `protobuf:"bytes,2,opt,name=val,proto3" json:"val,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *KeyVal) Reset()         { *m = KeyVal{} }
+func (m *KeyVal) String() string { return proto.CompactTextString(m) }
+func (*KeyVal) ProtoMessage()    {}
+func (*KeyVal) Descriptor() ([]byte, []int) {
+	return fileDescriptor_ec7364b073ade720, []int{5}
+}
+
+func (m *KeyVal) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_KeyVal.Unmarshal(m, b)
+}
+func (m *KeyVal) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_KeyVal.Marshal(b, m, deterministic)
+}
+func (m *KeyVal) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_KeyVal.Merge(m, src)
+}
+func (m *KeyVal) XXX_Size() int {
+	return xxx_messageInfo_KeyVal.Size(m)
+}
+func (m *KeyVal) XXX_DiscardUnknown() {
+	xxx_messageInfo_KeyVal.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_KeyVal proto.InternalMessageInfo
+
+func (m *KeyVal) GetKey() string {
+	if m != nil {
+		return m.Key
+	}
+	return ""
+}
+
+func (m *KeyVal) GetVal() string {
+	if m != nil {
+		return m.Val
+	}
+	return ""
+}
+
+type TransferKeysRequest struct {
+	FromId               string   `protobuf:"bytes,1,opt,name=from_id,json=fromId,proto3" json:"from_id,omitempty"`
+	ToNode               *Node    `protobuf:"bytes,2,opt,name=to_node,json=toNode,proto3" json:"to_node,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *TransferKeysRequest) Reset()         { *m = TransferKeysRequest{} }
+func (m *TransferKeysRequest) String() string { return proto.CompactTextString(m) }
+func (*TransferKeysRequest) ProtoMessage()    {}
+func (*TransferKeysRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_ec7364b073ade720, []int{6}
+}
+
+func (m *TransferKeysRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_TransferKeysRequest.Unmarshal(m, b)
+}
+func (m *TransferKeysRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_TransferKeysRequest.Marshal(b, m, deterministic)
+}
+func (m *TransferKeysRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_TransferKeysRequest.Merge(m, src)
+}
+func (m *TransferKeysRequest) XXX_Size() int {
+	return xxx_messageInfo_TransferKeysRequest.Size(m)
+}
+func (m *TransferKeysRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_TransferKeysRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_TransferKeysRequest proto.InternalMessageInfo
+
+func (m *TransferKeysRequest) GetFromId() string {
+	if m != nil {
+		return m.FromId
+	}
+	return ""
+}
+
+func (m *TransferKeysRequest) GetToNode() *Node {
+	if m != nil {
+		return m.ToNode
+	}
+	return nil
+}
+
 func init() {
 	proto.RegisterType((*ID)(nil), "chord.ID")
 	proto.RegisterType((*Node)(nil), "chord.Node")
 	proto.RegisterType((*Nodes)(nil), "chord.Nodes")
+	proto.RegisterType((*Key)(nil), "chord.Key")
+	proto.RegisterType((*Val)(nil), "chord.Val")
+	proto.RegisterType((*KeyVal)(nil), "chord.KeyVal")
+	proto.RegisterType((*TransferKeysRequest)(nil), "chord.TransferKeysRequest")
 }
 
 func init() {
@@ -156,14 +332,21 @@ func init() {
 }
 
 var fileDescriptor_ec7364b073ade720 = []byte{
-	// 144 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xd2, 0x2e, 0x4e, 0x2d, 0x2a,
-	0xcb, 0x4c, 0x4e, 0x2d, 0xd6, 0x4f, 0xce, 0xc8, 0x2f, 0x4a, 0x89, 0x87, 0x72, 0xa1, 0xbc, 0xdc,
-	0xd4, 0xe2, 0xe2, 0xc4, 0xf4, 0xd4, 0x62, 0xbd, 0x82, 0xa2, 0xfc, 0x92, 0x7c, 0x21, 0x56, 0xb0,
-	0xa8, 0x92, 0x08, 0x17, 0x93, 0xa7, 0x8b, 0x10, 0x1f, 0x17, 0x53, 0x66, 0x8a, 0x04, 0xa3, 0x02,
-	0xa3, 0x06, 0x67, 0x10, 0x53, 0x66, 0x8a, 0x92, 0x1a, 0x17, 0x8b, 0x5f, 0x7e, 0x4a, 0x2a, 0xba,
-	0x38, 0x98, 0x5f, 0x20, 0xc1, 0x04, 0xe5, 0x17, 0x28, 0x19, 0x71, 0xb1, 0x82, 0xd4, 0x15, 0x0b,
-	0x69, 0x72, 0x71, 0xe6, 0xe5, 0xa7, 0xa4, 0x3a, 0x16, 0x15, 0x25, 0x56, 0x4a, 0x30, 0x2a, 0x30,
-	0x6b, 0x70, 0x1b, 0x71, 0xeb, 0x81, 0x6d, 0xd0, 0x03, 0x29, 0x08, 0x42, 0xc8, 0x26, 0xb1, 0x81,
-	0xed, 0x37, 0x06, 0x04, 0x00, 0x00, 0xff, 0xff, 0x53, 0xcc, 0xaa, 0x52, 0xae, 0x00, 0x00, 0x00,
+	// 243 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x5c, 0x90, 0x51, 0x4b, 0xc3, 0x30,
+	0x10, 0xc7, 0x69, 0xea, 0x3a, 0x76, 0x03, 0x91, 0x28, 0xac, 0x8f, 0x23, 0x88, 0x4c, 0x94, 0x0a,
+	0xf3, 0x13, 0x08, 0xbe, 0x8c, 0x82, 0x0f, 0x65, 0xf8, 0x5a, 0xe2, 0x72, 0xd3, 0x62, 0xb7, 0xd4,
+	0x5c, 0x2d, 0xe4, 0xdb, 0xcb, 0x85, 0xcc, 0xb1, 0x3d, 0x25, 0xbf, 0xfb, 0xfd, 0x73, 0xb9, 0x04,
+	0x1e, 0x08, 0xdd, 0xd0, 0x6c, 0x90, 0x9e, 0x36, 0x5f, 0xd6, 0x99, 0x3a, 0x62, 0xa4, 0x1d, 0x12,
+	0xe9, 0x4f, 0xa4, 0xa2, 0x73, 0xb6, 0xb7, 0x72, 0x14, 0xaa, 0xea, 0x06, 0xc4, 0xea, 0x55, 0x5e,
+	0x82, 0x68, 0x4c, 0x9e, 0xcc, 0x93, 0xc5, 0xa4, 0x12, 0x8d, 0x51, 0x77, 0x70, 0xf1, 0x66, 0x0d,
+	0x9e, 0xd7, 0x03, 0x77, 0xb9, 0x88, 0xdc, 0xa9, 0x25, 0x8c, 0x38, 0x47, 0xf2, 0x1e, 0x26, 0x7b,
+	0x6b, 0xf0, 0xc5, 0x39, 0xed, 0xf3, 0x64, 0x9e, 0x2e, 0xa6, 0xcb, 0x69, 0x11, 0x6e, 0x28, 0x38,
+	0x50, 0x1d, 0xad, 0x9a, 0x41, 0x5a, 0xa2, 0x97, 0x57, 0x90, 0x7e, 0xa3, 0x8f, 0xbd, 0x79, 0xcb,
+	0xe2, 0x5d, 0xb7, 0x2c, 0x06, 0xdd, 0x1e, 0xc4, 0xa0, 0x5b, 0xf5, 0x08, 0x59, 0x89, 0x3e, 0xba,
+	0xd3, 0x43, 0x87, 0xb4, 0x38, 0xa6, 0xd7, 0x70, 0xbd, 0x76, 0x7a, 0x4f, 0x5b, 0x74, 0x25, 0x7a,
+	0xaa, 0xf0, 0xe7, 0x17, 0xa9, 0x97, 0x33, 0x18, 0x6f, 0x9d, 0xdd, 0xd5, 0xff, 0xef, 0xc9, 0x18,
+	0x57, 0x46, 0xde, 0xc2, 0xb8, 0xb7, 0x35, 0xcf, 0x17, 0xba, 0x9c, 0x0d, 0x9e, 0xf5, 0x96, 0xd7,
+	0x8f, 0x2c, 0xfc, 0xda, 0xf3, 0x5f, 0x00, 0x00, 0x00, 0xff, 0xff, 0xf7, 0xea, 0x69, 0xef, 0x64,
+	0x01, 0x00, 0x00,
 }
