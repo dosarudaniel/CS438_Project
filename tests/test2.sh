0ad95c28de5061a6aa5316a3a4de778055f08366
dosarudaniel <dosarudaniel@gmail.com>
2020-01-30 13:59:37 +0100

    Small improvements for test2

--- tests/test2.sh
+++ tests/test2.sh
@@ -6,13 +6,14 @@ cd ..
 
 go build
 
+# Removed current file1.txt for fair results
 rm _download/file1.txt
 
 xterm -title "Node Sender 219 " -hold -e "./CS438_Project -name A -ipAddr=127.0.0.1:5000 -create -m 8 -v"  &
 sleep 1
 cd client
 go build
-./client -PeersterAddress=127.0.0.1:5000 -command=download -file=file1.txt -nameToStore=file1.txt -ID=f98eeff24e2fced1a1336182a3e8775326262914cc4087066d9346431795ccdb -v
+./client -PeersterAddress=127.0.0.1:5000 -command=download -file=file1.txt -nameToStore=file1.txt -ID=db -v
 cd ..
 echo "Diference between uploaded and downloaded:"
 diff _upload/file1.txt _download/file1.txt
